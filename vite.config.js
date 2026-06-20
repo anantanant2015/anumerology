831d03e2db1197e1d6f6521933a035ceeac4bb7e
anantanant2015 <anantanant2015@gmail.com>
2026-06-20 15:16:21 +0530

    Updates config

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/anumerology/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
